01a91da4000af40971ab635b10d177253b8d70c3
Gonz8088 <gonzalesp3907@uhcl.edu>
2019-03-15 11:18:40 -0500

    added loop 1;

--- src/com/company/Midterm01.java
+++ src/com/company/Midterm01.java
@@ -3,5 +3,12 @@ package com.company;
 public class Midterm01 {
     public static void main(String[] args){
         double x = 2;
+
+        // loop 1
+
+        while(x < 11){
+            System.out.println(1/x);
+            x++;
+        }
     }
 }
